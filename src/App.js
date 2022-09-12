8fae34681b073b85a6d385bdff193a545bcb7abf
Jinnie-kim <hjk7639@gmail.com>
2022-09-12 17:39:36 +0900

    use router to make component url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  return <div></div>;
-}
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-export default App;
+import Nav from './components/Nav';
+import Home from './pages/Home/Home';
+import Join from './pages/Join/Join';
+import Login from './pages/Login/Login';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/join" element={<Join />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
