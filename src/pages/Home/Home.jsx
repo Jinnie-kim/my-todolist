9b62736ec375d0ba1f2028edd5ce145bc77a9109
Jinnie-kim <hjk7639@gmail.com>
2022-09-16 11:30:10 +0900

    add relevant components to Home component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,25 @@
+import { TodoForm } from './TodoForm';
+import { TodoSection } from './TodoSection';
+
+import styled from 'styled-components';
+
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <HomeSeciton>
+      <TodoForm />
+      <TodoSection />
+    </HomeSeciton>
+  );
 }
+
+const HomeSeciton = styled.section`
+  box-sizing: border-box;
+  width: 60%;
+  max-width: 650px;
+  height: 600px;
+  margin: 50px auto 0;
+  padding: 20px;
+  border: 2px solid #6a4c93;
+  text-align: center;
+  border-radius: 20px;
+`;
