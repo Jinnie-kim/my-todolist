4eb78419fe1748b7f96e18bbd330b6d7655fda49
Jinnie-kim <hjk7639@gmail.com>
2022-09-22 00:42:20 +0900

    use token state in App.js to set redirect path

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Reset } from 'styled-reset';
 
 import Nav from './components/Nav';
 import Home from './pages/Home/Home';
 import Join from './pages/Join/Join';
 import Login from './pages/Login/Login';
+import { useGlobalContext } from './hooks/useGlobalContext';
 
 export default function App() {
+  const { token } = useGlobalContext();
   return (
     <BrowserRouter>
-    <Reset />
+      <Reset />
       <Nav />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/join" element={<Join />} />
+        <Route
+          path="/"
+          element={token ? <Home /> : <Navigate replace={true} to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={!token ? <Login /> : <Navigate replace={true} to="/" />}
+        />
+        <Route
+          path="/join"
+          element={!token ? <Join /> : <Navigate replace={true} to="/" />}
+        />
       </Routes>
     </BrowserRouter>
   );
