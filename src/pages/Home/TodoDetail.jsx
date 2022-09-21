59dc633ae2414800ce48a725731539d5499a6429
Jinnie-kim <hjk7639@gmail.com>
2022-09-21 22:40:12 +0900

    add EditModal code & todo edit axios request code

--- src/pages/Home/TodoDetail.jsx
+++ src/pages/Home/TodoDetail.jsx
@@ -6,6 +6,7 @@ import styled from 'styled-components';
 export const TodoDetail = () => {
   const [title, setTitle] = useState('');
   const [detail, setDetail] = useState('');
+  const [show, setShow] = useState(false);
   const { id } = useGlobalContext();
   const token = localStorage.getItem('token');
 
@@ -42,6 +43,42 @@ export const TodoDetail = () => {
     }
   };
 
+  const showModalHandler = () => {
+    setShow(true);
+  };
+
+  const hideModalHandler = () => {
+    setShow(false);
+  };
+
+  const newTodoHandler = (event) => {
+    if (event.target.id === 'title') {
+      setTitle(event.target.value);
+    }
+    if (event.target.id === 'detail') {
+      setDetail(event.target.value);
+    }
+  };
+
+  const onTodoEditHandler = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.put(
+        `http://localhost:8080/todos/${id}`,
+        {
+          title: title,
+          content: detail,
+        },
+        {
+          headers: {
+            Authorization: token,
+          },
+        }
+      );
+      setShow(false);
+    } catch (error) {}
+  };
+
   return (
     <Detail>
       {id && (
@@ -49,11 +86,39 @@ export const TodoDetail = () => {
           <h1>{title}</h1>
           <p>{detail}</p>
           <div>
-            <button>EDIT</button>
+            <button onClick={showModalHandler}>EDIT</button>
             <button onClick={onTodoDeleteHandler}>DEL</button>
           </div>
         </>
       )}
+      {
+        <EditModal isTrue={show}>
+          <header>
+            <h2>Rewrite your todo</h2>
+          </header>
+          <form>
+            <label htmlFor="title">TODO</label>
+            <input
+              type="text"
+              id="title"
+              value={title}
+              onChange={newTodoHandler}
+            />
+
+            <label htmlFor="detail">DETAIL</label>
+            <input
+              type="text"
+              id="detail"
+              value={detail}
+              onChange={newTodoHandler}
+            />
+          </form>
+          <footer>
+            <button onClick={hideModalHandler}>UNDO</button>
+            <button onClick={onTodoEditHandler}>DONE</button>
+          </footer>
+        </EditModal>
+      }
     </Detail>
   );
 };
@@ -96,5 +161,70 @@ const Detail = styled.div`
 
   > div > button + button {
     margin-left: 15px;
+  }
+`;
+
+const EditModal = styled.section`
+  display: ${(props) => (props.isTrue ? 'block' : 'none')};
+  position: fixed;
+  top: 30vh;
+  left: 50%;
+  width: 60%;
+  transform: translateX(-50%);
+  background-color: #ffca3a;
+  border-radius: 15px;
+  overflow: hidden;
+  > header {
+    background-color: #ff595e;
+    padding: 1rem;
+    > h2 {
+      font-size: 24px;
+      font-weight: 700;
+      color: #ffca3a;
+    }
+  }
+
+  > form {
+    padding: 1rem;
+    text-align: left;
+
+    > label {
+      display: inline-block;
+      margin: 20px 0 10px;
+      font-weight: 700;
+      &:first-child {
+        margin-top: 0;
+      }
+    }
+
+    > input {
+      box-sizing: border-box;
+      width: 100%;
+      padding: 10px;
+      outline: none;
+      border: 2px solid #ffca3a;
+      border-radius: 10px;
+      &:hover,
+      &:focus {
+        border: 2px solid #ff595e;
+      }
+    }
+  }
+
+  > footer {
+    padding: 1rem;
+    display: flex;
+    justify-content: center;
+    gap: 10px;
+
+    > button {
+      padding: 5px 10px;
+      font-weight: 700;
+      border: 2px solid #ff595e;
+      background-color: #ff595e;
+      color: #ffca3a;
+      border-radius: 10px;
+      cursor: pointer;
+    }
   }
 `;
